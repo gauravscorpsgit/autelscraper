b78902653800513b6fc977862b3363136e343591
Gaurav Chawla <gauravscorpsgit@users.noreply.github.com>
2018-05-30 16:53:48 +0530

    Update mws.js

--- mws.js
+++ mws.js
@@ -17,7 +17,7 @@ const mws = new MWSClient({
   }
 });
 	const Feeds = require('mws-api/feeds');
-mws.Feeds.SubmitFeed({    FeedContent: Feeds._POST_ORDER_FULFILLMENT_DATA_({      MessageID: "1",      MerchantOrderID: "114-9872396-6852265",      MerchantFulfillmentID: "1234567",      FulfillmentDate: '2002-05-01T15:36:33-08:00',      CarrierCode: 'DHL Paket',      ShippingMethod: 'DHL Paket',      ShipperTrackingNumber: '1234567890'    }),    FeedType: '_POST_ORDER_FULFILLMENT_DATA_'  })  .then((res) => {    console.log(res)  })  .catch((e) => {    console.log('error', e);  });
+mws.Feeds.SubmitFeed({    FeedContent: Feeds._POST_ORDER_FULFILLMENT_DATA_({      MessageID: "1",      MerchantOrderID: "112-9661766-6523437",      MerchantFulfillmentID: "1234567",      FulfillmentDate: '2002-05-01T15:36:33-08:00',      CarrierCode: 'DHL Paket',      ShippingMethod: 'DHL Paket',      ShipperTrackingNumber: '1234567890'    }),    FeedType: '_POST_ORDER_FULFILLMENT_DATA_'  })  .then((res) => {    console.log(res)  })  .catch((e) => {    console.log('error', e);  });
 	
 
 });
